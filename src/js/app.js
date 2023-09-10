3b9ac781376d1ec310124d0db417af7041cc1e23
Camiboro <130386618+Camiboro@users.noreply.github.com>
2023-09-10 16:28:41 +0000

    postcard

--- src/js/app.js
+++ src/js/app.js
@@ -4,23 +4,51 @@ import "../style/index.css";
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
  * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
+    {function render(variables = {}) {
+  const {
+    includeCover = true,
+    background = null,
+    avatarURL = null,
+    socialMediaPosition = "right",
+    twitter = null,
+    github = null,
+    linkedin = null,
+    instagram = null,
+    name = null,
+    lastname = null,
+    role = null,
+    country = null,
+    city = null
+  } = variables;
 
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
+  const coverHTML = includeCover && background ? `<div class="cover"><img src="${background}" /></div>` : '';
+  const avatarHTML = avatarURL ? `<img src="${avatarURL}" class="photo" />` : '';
+  
+  const socialLinks = [
+    { platform: 'twitter', icon: 'fa-twitter', url: `https://twitter.com/${twitter}` },
+    { platform: 'github', icon: 'fa-github', url: `https://github.com/${github}` },
+    { platform: 'linkedin', icon: 'fa-linkedin', url: `https://linkedin.com/in/${linkedin}` },
+    { platform: 'instagram', icon: 'fa-instagram', url: `https://instagram.com/${instagram}` }
+  ].map(link => {
+    return link.platform && variables[link.platform] ? `<li><a href="${link.url}"><i class="${link.icon}"></i></a></li>` : '';
+  }).join('');
+
+  const html = `
+    <div class="widget">
+      ${coverHTML}
+      ${avatarHTML}
+      <h1>${name} ${lastname}</h1>
+      <h2>${role}</h2>
+      <h3>${city}, ${country}</h3>
+      <ul class="position-${socialMediaPosition}">
+        ${socialLinks}
+      </ul>
+    </div>
+  `;
+
+  document.querySelector("#widget_content").innerHTML = html;
+}
+}
  */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
